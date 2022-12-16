2aacb3d650b20e9178765f00ba05431a1c2eea7e
gustmd5944 <gustmd5944@gmail.com>
2022-12-16 19:38:28 +0900

    last last commit

--- src/shared/Routers.js
+++ src/shared/Routers.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../components/layout/Home";
-// import { Header } from "../components/layout/Header";
+import { Header } from "../components/layout/Header";
 
 export default function Hompage() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
